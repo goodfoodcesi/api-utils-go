476ec132bb9a9be5888aa0c7f70184e077264601
Théo TRUVELOT <theotruvelott@gmail.com>
2024-12-31 18:19:45 +0100

    add new order function

--- pkg/order/order.go
+++ pkg/order/order.go
@@ -1,5 +1,7 @@
 package order
 
+import "github.com/google/uuid"
+
 const (
 	Type         = "Order"
 	OrderCreated = "OrderCreated"
@@ -11,4 +13,14 @@ type Order struct {
 	CustomerID string `json:"customer_id"`
 	ShopID     string `json:"shop_id"`
 	DeliveryID string `json:"delivery_id"`
+}
+
+func NewOrder(orderID, customerID, shopID, deliveryID string) *Order {
+	return &Order{
+		ID:         uuid.NewString(),
+		OrderID:    orderID,
+		CustomerID: customerID,
+		ShopID:     shopID,
+		DeliveryID: deliveryID,
+	}
 }
