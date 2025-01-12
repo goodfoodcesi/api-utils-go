b3767bf419b86b3d8255304ac8478fcb6078f5cf
Théo TRUVELOT <theotruvelott@gmail.com>
2025-01-12 19:20:55 +0100

    edit uuid

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/goodfoodcesi/api-utils-go
 
 go 1.23.4
 
-require github.com/google/uuid v1.6.0
+require github.com/jackc/pgx/v5 v5.7.2
--- pkg/event/user_event.go
+++ pkg/event/user_event.go
@@ -1,28 +1,28 @@
 package event
 
 import (
-	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
 	"time"
 )
 
 type UserCreatedEvent struct {
-	ID        uuid.UUID `json:"id"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Role      string    `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        pgtype.UUID `json:"id"`
+	Email     string      `json:"email"`
+	FirstName string      `json:"first_name"`
+	LastName  string      `json:"last_name"`
+	Role      string      `json:"role"`
+	CreatedAt time.Time   `json:"created_at"`
 }
 
 type UserUpdatedEvent struct {
-	ID        uuid.UUID `json:"id"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Role      string    `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        pgtype.UUID `json:"id"`
+	Email     string      `json:"email"`
+	FirstName string      `json:"first_name"`
+	LastName  string      `json:"last_name"`
+	Role      string      `json:"role"`
+	CreatedAt time.Time   `json:"created_at"`
 }
 
 type UserDeletedEvent struct {
-	ID uuid.UUID `json:"id"`
+	ID pgtype.UUID `json:"id"`
 }
